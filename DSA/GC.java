927605122eeb402ef3f35e9eb7563c9deefed803
Mohit Baghel <mohit.mynt@perennialsys.com>
2025-08-12 19:30:07 +0530

    Garbage collector

---
+++ DSA/GC.java
@@ -0,0 +1,34 @@
+package DSA;
+
+public class GC {
+
+    public static void main(String[] args) {
+        
+
+        static int a = 10; // static variable are stored in the static memory area
+
+
+        int i = 20;  // instance variable are stored in the heap memory area
+    }
+
+
+
+    public void display(){
+        int s = 30; // local variable are stored in the stack memory area
+    }
+    
+
+
+
+   // System.gc()    // This method is used to suggest the JVM to perform garbage collection.
+}
+
+
+// heap
+// method
+// stacks
+// native methods stacks
+// PC register
+
+
+// 
